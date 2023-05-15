6b59e72365c4c64916a652d9427e2bea52cba43b
Lorenzo Ugoccioni s315734 <s315734@studenti.polito.it>
2023-05-15 16:34:42 +0200

    getGroup - not finished

--- code/controllers/users.js
+++ code/controllers/users.js
@@ -131,6 +131,14 @@ export const getGroups = async (req, res) => {
  */
 export const getGroup = async (req, res) => {
   try {
+    const groupName = req.params.name;
+    const groupInfo = await Group.findById({ name: groupName });
+   if(!groupInfo){
+     return res.status(401).json({message: `The group ${groupName} doesn't exist !`});
+   }else{
+         res.status(200).json({ data: {param: groupInfo} });
+   }
+   
   } catch (err) {
     res.status(500).json(err.message);
   }
@@ -147,49 +155,8 @@ export const getGroup = async (req, res) => {
     - error 401 is returned if the group does not exist
     - error 401 is returned if all the `memberEmails` either do not exist or are already in a group
  */
-/*export const addToGroup = async (req, res) => {
-  try {
-  } catch (err) {
-    res.status(500).json(err.message);
-  }
-};
-*/
 export const addToGroup = async (req, res) => {
   try {
-    const { group, memberEmails } = req.body;
-
-    const existingGroup = await Group.findOne({ name: group });
-    if (!existingGroup) {
-      return res.status(401).json({ message: "Group does not exist" });
-    }
-
-    const membersNotFound = [];
-    const alreadyInGroup = [];
-
-    for (let email of memberEmails) {
-      const user = await User.findOne({ email });
-
-      if (!user) {
-        membersNotFound.push(email);
-      } else if (existingGroup.members.some((member) => member.email === email)) {
-        alreadyInGroup.push(email);
-      } else {
-        existingGroup.members.push({ email, username: user.username });
-      }
-    }
-
-    await existingGroup.save();
-
-    const responseData = {
-      group: {
-        name: existingGroup.name,
-        members: existingGroup.members.map((member) => member.email),
-      },
-      alreadyInGroup,
-      membersNotFound,
-    };
-
-    res.status(200).json(responseData);
   } catch (err) {
     res.status(500).json(err.message);
   }
