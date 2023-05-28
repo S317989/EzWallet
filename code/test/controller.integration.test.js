a8c3541d3d4f993fb8d3c4924dec28ff88848f32
Lorenzo Ugoccioni s315734 <s315734@studenti.polito.it>
2023-05-29 00:22:51 +0200

    Test for category methods - check the syntax

--- code/test/controller.integration.test.js
+++ code/test/controller.integration.test.js
@@ -3,6 +3,7 @@ import { app } from "../app";
 import { categories, transactions } from "../models/model";
 import mongoose, { Model } from "mongoose";
 import dotenv from "dotenv";
+import { User } from "../models/User";
 
 dotenv.config();
 
@@ -14,6 +15,8 @@ beforeAll(async () => {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
+
+  
 });
 
 afterAll(async () => {
@@ -22,32 +25,426 @@ afterAll(async () => {
 });
 
 describe("createCategory", () => {
-  test("Dummy test, change it", () => {
-    expect(true).toBe(true);
+/**How can I handle the authentication part by the Admin User? */
+
+  beforeEach(async ()=>{
+    /*const Admin ={
+      username:"Admin",
+      email:"AdminAdmin@controller.com",
+      password:"Admin1",
+      role: "Admin",
+    }
+    await User.create(Admin);*/
+
+    await categories.deleteMany({});
+  });
+  test("New category inserted", (done) => {
+    const category = {
+      type: "TestType",
+      color: "TestColor",
+    };
+
+    request(app)
+      .post("/api/categories")
+      .send(category)
+      .then((response)=>{
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty("data");
+        expect(response.body.data).toHaveProperty("type");
+        expect(response.body.data.type).toEqual("TestType");
+        expect(response.body.data).toHaveProperty("color");
+        expect(response.body.data.color).toEqual("TestColor");
+        done();
+      })
+      .catch((err) => done(err));
+  });
+
+  test("Not enough attributes", (done) => {
+    const category1 = {
+      type: "TestType",
+    };
+
+    request(app)
+      .post("/api/categories")
+      .send(category)
+      .then((response)=>{
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty("error");
+        expect(response.body).toEqual({
+          error: expect.stringContaining(`is missing`),
+        });
+        done();
+      })
+      .catch((err) => done(err));
+  });
+  test("Not enough attributes", (done) => {
+    const category = {
+      color: "TestColor",
+    };
+
+    request(app)
+      .post("/api/categories")
+      .send(category)
+      .then((response)=>{
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty("error");
+        expect(response.body).toEqual({
+          error: expect.stringContaining(`is missing`),
+        });
+        done();
+      })
+      .catch((err) => done(err));
+  });
+  test("Type attributes is empty ", (done) => {
+    const category = {
+      type: "",
+      color: "TestColor",
+    };
+
+    request(app)
+      .post("/api/categories")
+      .send(category1)
+      .then((response)=>{
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty("error");
+        expect(response.body).toEqual({
+          error: expect.stringContaining(`is empty`),
+        });
+        done();
+      })
+      .catch((err) => done(err));
+  });
+  
+  test("Color attributes is empty ", (done) => {
+    const category = {
+      type: "TestType",
+      color: "",
+    };
+
+    request(app)
+      .post("/api/categories")
+      .send(category2)
+      .then((response)=>{
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty("error");
+        expect(response.body).toEqual({
+          error: expect.stringContaining(`is empty`),
+        });
+        done();
+      })
+      .catch((err) => done(err));
+  });
+
+  test("Category already exist", (done) => {
+    const category = {
+      type: "TestType",
+      color: "TestColor",
+    };
+
+    categories.create(category).then(() => {
+      request(app)
+        .post("/api/categories")
+        .send(category)
+        .then((response)=>{
+          expect(response.status).toBe(400);
+          expect(response.body).toHaveProperty("error");
+          expect(response.body).toEqual({
+            error: expect.stringContaining(`already exist`),
+          });
+          done();
+        })
+        .catch((err) => done(err));
+      });
+  });
+  
+  test("User not Authorized", (done) => {
+    const category = {
+      type: "TestType",
+      color: "TestColor",
+    };
+
+    request(app)
+      .post("/api/categories")
+      .send(category)
+      .then((response)=>{
+        expect(response.status).toBe(401);
+        expect(response.body).toHaveProperty("error");
+          expect(response.body).toEqual({
+            error: expect.stringContaining(`unauthorized`),
+          });
+          done();
+      })
+      .catch((err) => done(err));
   });
 });
 
 describe("updateCategory", () => {
-  test("Dummy test, change it", () => {
-    expect(true).toBe(true);
+  beforeAll(async () =>{
+    const standardValue ={
+      type:"TestType",
+      color:"TestColor",
+    }
+    await categories.deleteMany({});
+    await categories.create(standardValue);
   });
+
+  test("Category update - Success", () => {
+    const newValue ={
+      type:"NewType",
+      color: "NewColor",
+    }
+
+    request(app)
+      .patch("api/categories/:type")
+      .send(newValue)
+      .then((response) => {
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty("data");
+        expect(response.body.data).toHaveProperty("message");
+        expect(response.body.data.message).toContain("updated to");
+        expect(response.body.data).toHaveProperty("count");
+        expect(response.body.data.count).toBeGreaterThanOrEqual(0);
+        done();
+      })
+      .catch((err) => done(err));
+
+  });
+
+  test("Category update - Category doesn't exist", () => {
+    const newValue ={
+      type:"NewType",
+      color: "NewColor",
+    }
+
+    request(app)
+      .patch("api/categories/:type")
+      .send(newValue)
+      .then((response) => {
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty("data");
+        expect(response.body.data).toHaveProperty("message");
+        expect(response.body.data.message).toContain("updated to");
+        expect(response.body.data).toHaveProperty("count");
+        expect(response.body.data.count).toBeGreaterThanOrEqual(0);
+        done();
+      })
+      .catch((err) => done(err));
+  });
+  
+  test("Category update - Invalid Values", () => {
+    const newValue ={
+      type:"NewType",
+      color: "NewColor",
+    }
+
+    request(app)
+      .patch("api/categories/:type")
+      .send(newValue)
+      .then((response) => {
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty("data");
+        expect(response.body.data).toHaveProperty("message");
+        expect(response.body.data.message).toContain("updated to");
+        expect(response.body.data).toHaveProperty("count");
+        expect(response.body.data.count).toBeGreaterThanOrEqual(0);
+        done();
+      })
+      .catch((err) => done(err));
+  });
+  
+  test("Category update - Not an Admin", () => {
+    const newValue ={
+      type:"NewType",
+      color: "NewColor",
+    }
+
+    request(app)
+      .patch("api/categories/:type")
+      .send(newValue)
+      .then((response) => {
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty("data");
+        expect(response.body.data).toHaveProperty("message");
+        expect(response.body.data.message).toContain("updated to");
+        expect(response.body.data).toHaveProperty("count");
+        expect(response.body.data.count).toBeGreaterThanOrEqual(0);
+        done();
+      })
+      .catch((err) => done(err));
+  });
+
 });
 
 describe("deleteCategory", () => {
-  test("Dummy test, change it", () => {
-    expect(true).toBe(true);
+  beforeEach(async () => {
+    const category1 ={
+      type:"NewType1",
+      color: "NewColor1",
+    }
+    const category2 ={
+      type:"NewType2",
+      color: "NewColor2",
+    }
+    const category3 ={
+      type:"NewType3",
+      color: "NewColor3",
+    }
+    const category4 ={
+      type:"NewType4",
+      color: "NewColor4",
+    }
+    const category5 ={
+      type:"NewType5",
+      color: "NewColor5",
+    }
+    await categories.deleteMany({});
+    await categories.insertMany([category1, category2, category3, category4, category5]);
+  });
+
+  test("Delete Category - Delete 1", () => {
+    const categoryToBeDeleted =[{type:"NewType1"}];
+
+    request(app)
+      .delete("api/categories")
+      .send(categoryToBeDeleted)
+      .then((response) => {
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty("data");
+        expect(response.body.data).toHaveProperty("message");
+        expect(response.body.data.message).toContain("successfully deleted");
+        expect(response.body.data).toHaveProperty("count");
+        expect(response.body.data.count).toBeGreaterThanOrEqual(0);
+        done();
+      })
+      .catch((err) => done(err));
+  });
+
+  test("Delete Category - Delete Many", () => {
+    const categoryToBeDeleted =[{type:"NewType1"},{type:"NewType2"},{type:"NewType3"}];
+
+    request(app)
+      .delete("api/categories")
+      .send(categoryToBeDeleted)
+      .then((response) => {
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty("data");
+        expect(response.body.data).toHaveProperty("message");
+        expect(response.body.data.message).toContain("successfully deleted");
+        expect(response.body.data).toHaveProperty("count");
+        expect(response.body.data.count).toBeGreaterThanOrEqual(0);
+        done();
+      })
+      .catch((err) => done(err));
+  });
+/**Need to do the error test */
+  test("Delete Category - r", () => {
+    const categoryToBeDeleted =[{type:"NewType1"},{type:"NewType2"},{type:"NewType3"}];
+
+    request(app)
+      .delete("api/categories")
+      .send(categoryToBeDeleted)
+      .then((response) => {
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty("data");
+        expect(response.body.data).toHaveProperty("message");
+        expect(response.body.data.message).toContain("successfully deleted");
+        expect(response.body.data).toHaveProperty("count");
+        expect(response.body.data.count).toBeGreaterThanOrEqual(0);
+        done();
+      })
+      .catch((err) => done(err));
+  });
+
+  test("Delete Category - Not an Admin", () => {
+    const newValue ={
+      type:"NewType",
+    }
+
+    request(app)
+      .get("api/categories")
+      .send()
+      .then((response) => {
+        expect(response.status).toBe(401);
+        expect(response.body).toHaveProperty("error");
+        expect(response.body.data).toHaveProperty("message");
+        expect(response.body.error).toContain("unauthorized");
+        done();
+      })
+      .catch((err) => done(err));
   });
 });
 
 describe("getCategories", () => {
-  test("Dummy test, change it", () => {
-    expect(true).toBe(true);
+
+  beforeAll(async () => {
+    const category1 ={
+      type:"NewType1",
+      color: "NewColor1",
+    }
+    const category2 ={
+      type:"NewType2",
+      color: "NewColor2",
+    }
+    const category3 ={
+      type:"NewType3",
+      color: "NewColor3",
+    }
+    const category4 ={
+      type:"NewType4",
+      color: "NewColor4",
+    }
+    const category5 ={
+      type:"NewType5",
+      color: "NewColor5",
+    }
+    await categories.deleteMany({});
+    await categories.insertMany([category1, category2, category3, category4, category5]);
+  });
+  test("Get Category - Succesfully", () => {
+    request(app)
+      .get("api/categories")
+      .send()
+      .then((response) => {
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty("data");
+        expect(response.body.data).not.toBeFalsy();
+        done();
+      })
+      .catch((err) => done(err));
+  });
+
+  test("Get Category - Not an Admin", () => {
+    request(app)
+      .get("api/categories/")
+      .send()
+      .then((response) => {
+        expect(response.status).toBe(401);
+        expect(response.body).toHaveProperty("error");
+        expect(response.body.data).toHaveProperty("message");
+        expect(response.body.error).toContain("unauthorized");
+        done();
+      })
+      .catch((err) => done(err));
   });
 });
 
 describe("createTransaction", () => {
-  test("Dummy test, change it", () => {
-    expect(true).toBe(true);
+  
+  beforeEach(async()=>{
+    await transactions.deleteMany({});
+  });
+  test("Transaction created", () => {
+    request(app)
+      .post("api/users/:username/transaction")
+      .send()
+      .then((response) => {
+        expect(response.status).toBe(401);
+        expect(response.body).toHaveProperty("error");
+        expect(response.body.data).toHaveProperty("message");
+        expect(response.body.error).toContain("unauthorized");
+        done();
+      })
+      .catch((err) => done(err));
   });
 });
 
