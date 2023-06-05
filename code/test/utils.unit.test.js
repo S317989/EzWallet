f0fdc197ad541f0d3de23c33a1827cee5bce2fa3
Andrea Alessandro Gaudino <gaudino00.ag@gmail.com>
2023-06-05 17:53:30 +0200

    Utils unit test done

--- code/test/utils.unit.test.js
+++ code/test/utils.unit.test.js
@@ -1,30 +1,22 @@
 import * as utilsMethods from "../controllers/utils.js";
 import request from "supertest";
 import { app } from "../app";
+import dotenv from "dotenv";
 
 const jwt = require("jsonwebtoken");
 
+dotenv.config();
+
+let mockRequest, mockResponse;
+
 describe("handleDateFilterParams", () => {
-  let mockRequest;
-  let mockResponse;
-
-  beforeEach(() => {
+  test("HandleDateFilterParams - upTo and from cannot be together", (done) => {
     mockRequest = {
-      query: {},
-    };
-
-    mockResponse = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
-      cookie: jest.fn(),
-    };
-  });
-
-  test("should throw an error if date is provided with from or upTo", (done) => {
-    mockRequest.query = {
-      date: "2023-05-31",
-      from: "2023-05-01",
-      upTo: "2023-05-31",
+      query: {
+        date: "2023-05-31",
+        from: "2023-05-01",
+        upTo: "2023-05-31",
+      },
     };
 
     expect(() => {
@@ -33,9 +25,11 @@ describe("handleDateFilterParams", () => {
     done();
   });
 
-  test("should throw an error if date is not in the format YYYY-MM-DD", async () => {
-    mockRequest.query = {
-      date: "2023/05/31",
+  test("HandleDateFilterParams -  Date not in format YYYY-MM-DD", async () => {
+    mockRequest = {
+      query: {
+        date: "2023/05/31",
+      },
     };
 
     expect(() => {
@@ -43,9 +37,11 @@ describe("handleDateFilterParams", () => {
     }).toThrow(Error);
   });
 
-  test("should return the correct filter object when only date is provided", async () => {
-    mockRequest.query = {
-      date: "2023-05-31",
+  test("HandleDateFilterParams - Success with date filter", async () => {
+    mockRequest = {
+      query: {
+        date: "2023-05-31",
+      },
     };
 
     const expectedFilter = {
@@ -54,15 +50,16 @@ describe("handleDateFilterParams", () => {
       },
     };
 
-    expect(() => {
-      utilsMethods.handleDateFilterParams(mockRequest);
-    }).toThrow(expectedFilter);
+    expect(utilsMethods.handleDateFilterParams(mockRequest)).toEqual(
+      expectedFilter
+    );
   });
 
-  test("should return the correct filter object when from  is provided", async () => {
-    mockRequest.query = {
-      date: "2023-05-31",
-      from: "2023-05-01",
+  test("HandleDateFilterParams - Success with from filter", async () => {
+    mockRequest = {
+      query: {
+        from: "2023-05-01",
+      },
     };
 
     const expectedFilter = {
@@ -70,16 +67,16 @@ describe("handleDateFilterParams", () => {
         $gte: new Date(mockRequest.query["from"]),
       },
     };
-
-    expect(() => {
-      utilsMethods.handleDateFilterParams(mockRequest);
-    }).toThrow(expectedFilter);
+    expect(utilsMethods.handleDateFilterParams(mockRequest)).toEqual(
+      expectedFilter
+    );
   });
 
-  test("should return the correct filter object when upTo  is provided", async () => {
-    mockRequest.query = {
-      date: "2023-05-31",
-      upTo: "2023-05-01",
+  test("HandleDateFilterParams - Success with upTo filter", async () => {
+    mockRequest = {
+      query: {
+        upTo: "2023-05-01",
+      },
     };
 
     const upToDate = new Date(mockRequest.query["upTo"]);
@@ -91,27 +88,25 @@ describe("handleDateFilterParams", () => {
         $lte: upToDate,
       },
     };
+    /*
+      expect(() => {
+        utilsMethods.handleDateFilterParams(mockRequest);
+      }).not.toThrow(expectedFilter);
+    });
+    */
 
-    expect(() => {
-      utilsMethods.handleDateFilterParams(mockRequest);
-    }).toThrow(expectedFilter);
+    expect(utilsMethods.handleDateFilterParams(mockRequest)).toEqual(
+      expectedFilter
+    );
   });
 });
 
 describe("handleAmountFilterParams", () => {
-  let mockRequest;
-  let data;
-
-  beforeEach(() => {
+  test("HandleAmountFilterParams - Min amount not a number", () => {
     mockRequest = {
-      query: {},
-    };
-    data = {}; // Test data for the second argument of handleAmountFilterParams
-  });
-
-  test("should throw an error if minAmount is not a number", () => {
-    mockRequest.query = {
-      minAmount: "abc",
+      query: {
+        minAmount: "abc",
+      },
     };
 
     expect(() => {
@@ -119,9 +114,11 @@ describe("handleAmountFilterParams", () => {
     }).toThrow(Error);
   });
 
-  test("should throw an error if maxAmount is not a number", () => {
-    mockRequest.query = {
-      maxAmount: "def",
+  test("HandleAmountFilterParams - MaxAmount not a number", () => {
+    mockRequest = {
+      query: {
+        maxAmount: "def",
+      },
     };
 
     expect(() => {
@@ -129,10 +126,12 @@ describe("handleAmountFilterParams", () => {
     }).toThrow(Error);
   });
 
-  test("should return the correct filter object when minAmount and maxAmount are provided", () => {
-    mockRequest.query = {
-      minAmount: "100",
-      maxAmount: "500",
+  test("HandleAmountFilterParams - Success with minAmount and maxAmount filters", () => {
+    mockRequest = {
+      query: {
+        minAmount: "100",
+        maxAmount: "500",
+      },
     };
 
     const expectedFilter = {
@@ -142,14 +141,16 @@ describe("handleAmountFilterParams", () => {
       },
     };
 
-    expect(() => {
-      utilsMethods.handleAmountFilterParams(mockRequest, data);
-    }).toThrow(expectedFilter);
+    expect(utilsMethods.handleAmountFilterParams(mockRequest)).toEqual(
+      expectedFilter
+    );
   });
 
-  test("should return the correct filter object when only minAmount is provided", () => {
-    mockRequest.query = {
-      minAmount: "100",
+  test("HandleAmountFilterParams - Success with only minAmount filter", () => {
+    mockRequest = {
+      query: {
+        minAmount: "100",
+      },
     };
 
     const expectedFilter = {
@@ -158,14 +159,16 @@ describe("handleAmountFilterParams", () => {
       },
     };
 
-    expect(() => {
-      utilsMethods.handleAmountFilterParams(mockRequest, data);
-    }).toThrow(expectedFilter);
+    expect(utilsMethods.handleAmountFilterParams(mockRequest)).toEqual(
+      expectedFilter
+    );
   });
 
-  test("should return the correct filter object when only maxAmount is provided", () => {
-    mockRequest.query = {
-      maxAmount: "500",
+  test("HandleAmountFilterParams - Success with only maxAmount filter", () => {
+    mockRequest = {
+      query: {
+        maxAmount: "500",
+      },
     };
 
     const expectedFilter = {
@@ -174,8 +177,202 @@ describe("handleAmountFilterParams", () => {
       },
     };
 
-    expect(() => {
-      utilsMethods.handleAmountFilterParams(mockRequest, data);
-    }).toThrow(expectedFilter);
+    expect(utilsMethods.handleAmountFilterParams(mockRequest)).toEqual(
+      expectedFilter
+    );
+  });
+
+  describe("verifyAuth", () => {
+    let accessToken, refreshToken;
+
+    test("VerifyAuth - Success", () => {
+      accessToken = jwt.sign(
+        {
+          username: "User",
+          email: "user@email.com",
+          id: "1",
+          role: "Regular",
+        },
+        process.env.ACCESS_KEY,
+        { expiresIn: "1h" }
+      );
+
+      refreshToken = jwt.sign(
+        {
+          username: "User",
+          email: "user@email.com",
+          id: "1",
+          role: "Regular",
+        },
+        process.env.ACCESS_KEY,
+        { expiresIn: "7d" }
+      );
+
+      mockRequest = {
+        cookies: {
+          accessToken: accessToken,
+          refreshToken: refreshToken,
+        },
+      };
+
+      mockResponse = {
+        cookie: jest.fn(),
+        locals: {},
+      };
+
+      const result = utilsMethods.verifyAuth(mockRequest, mockResponse, {});
+
+      expect(result.flag).toBe(true);
+      expect(result.cause).toBe("Authorized");
+    });
+
+    test("VerifyAuth - Tokens missing", () => {
+      const mockReq = {
+        cookies: {},
+      };
+      const mockRes = {};
+
+      expect(utilsMethods.verifyAuth(mockReq, mockRes)).toEqual({
+        flag: false,
+        cause: "Unauthorized",
+      });
+    });
+
+    test("VerifyAuth - Tokens is missing information", () => {
+      const mockReq = {
+        cookies: {
+          accessToken: "mockAccessToken",
+          refreshToken: "mockRefreshToken",
+        },
+      };
+
+      jest
+        .spyOn(jwt, "verify")
+        .mockImplementationOnce((token, key) => {
+          if (token === "mockAccessToken") {
+            return {
+              username: "1",
+              email: "2",
+              role: "admin",
+            };
+          } else if (token === "mockRefreshToken") {
+            return {
+              email: "2",
+              role: "admin",
+            };
+          }
+        })
+        .mockImplementationOnce((token, key) => {
+          if (token === "mockAccessToken") {
+            return {
+              username: "1",
+              email: "2",
+              role: "admin",
+            };
+          } else if (token === "mockRefreshToken") {
+            return {
+              email: "2",
+              role: "admin",
+            };
+          }
+        });
+      const mockRes = {};
+      const mockInfo = {};
+
+      const result = utilsMethods.verifyAuth(mockReq, mockRes, mockInfo);
+      expect(result.flag).toBe(false);
+      expect(result.cause).toBe("Token is missing information");
+    });
+
+    test("VerifyAuth - Mismatched users", () => {
+      const mockReq = {
+        cookies: {
+          accessToken: "mockAccessToken",
+          refreshToken: "mockRefreshToken",
+        },
+      };
+
+      jest
+        .spyOn(jwt, "verify")
+        .mockImplementationOnce((token, key) => {
+          if (token === "mockAccessToken") {
+            return {
+              username: "1",
+              email: "2",
+              role: "admin",
+            };
+          } else if (token === "mockRefreshToken") {
+            return {
+              username: "12",
+              email: "2",
+              role: "admin",
+            };
+          }
+        })
+        .mockImplementationOnce((token, key) => {
+          if (token === "mockAccessToken") {
+            return {
+              username: "1",
+              email: "2",
+              role: "admin",
+            };
+          } else if (token === "mockRefreshToken") {
+            return {
+              username: "12",
+              email: "2",
+              role: "admin",
+            };
+          }
+        });
+      const decodedAccessToken = { username: "1", email: "2", role: "3" };
+      const decodedRefreshToken = { username: "12", email: "2", role: "A" };
+
+      const mockRes = {};
+      const mockInfo = {};
+
+      const result = utilsMethods.verifyAuth(mockReq, mockRes, mockInfo);
+
+      expect(result.flag).toBe(false);
+      expect(result.cause).toBe("Mismatched users");
+    });
+
+    test("VerifyAuth - AccessToken expired", () => {
+      refreshToken = jwt.sign(
+        {
+          username: "User",
+          email: "user@email.com",
+          id: "1",
+          role: "Regular",
+        },
+        process.env.ACCESS_KEY,
+        { expiresIn: "7d" }
+      );
+
+      mockRequest = {
+        cookies: {
+          accessToken: "expired-access-token",
+          refreshToken: refreshToken,
+        },
+      };
+
+      mockResponse = {
+        cookie: jest.fn(),
+        locals: {},
+      };
+
+      // Mock della funzione jwt.verify per il token scaduto
+      jest.spyOn(jwt, "verify").mockImplementationOnce((token, key) => {
+        throw new jwt.TokenExpiredError("Token expired");
+      });
+
+      const result = utilsMethods.verifyAuth(mockRequest, mockResponse, {});
+
+      expect(result.flag).toBe(true);
+      expect(result.cause).toBe("Authorized");
+
+      expect(mockResponse.locals.message).toBe(
+        "Access token has been refreshed. Remember to copy the new one in the headers of subsequent calls"
+      );
+    });
   });
 });
